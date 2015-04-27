231c7605056f139e1ed5f5ba54d87a859d56e1da
dog44045 <am021953@au.edu.tw>
2015-04-27 10:38:10 +0800

    A AM021953 張哲中提交ThreadA

--- Question3.java
+++ Question3.java
@@ -1,6 +1,16 @@
 public class Question3{
 	public statis void main(String[] args){
-		
-		
+		Thread m=Thread.currentThread();
+		ThreadA t1= new ThreadA();
+		t1.start();
+	}
+}
+
+class ThreadA extend Thread{
+	public void run(){
+		for (int i=0; i<1000; i++){
+			System.out.println("ThreadA");
+			try(this.sleep(1000);) catch (Exception e) ()
+ 		}	
 	}
 }
